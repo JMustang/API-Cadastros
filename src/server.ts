b5c8c4f23b55ff1d1784aa39994ba8f0a8c271f6
Junior <eecfredes@gmail.com>
2021-09-15 09:56:56 -0300

    feat:sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,11 @@
+import "reflect-metadata";
 import express from "express";
+
+import "./database";
 
 const PORT = "3000";
 
 const app = express();
-
-app.get("/teste", (req, res) => {
-  return res.send("Ola, dev!!");
-});
-
-app.post("/teste-post", (req, res) => {
-  return res.send("Ola API!!");
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
