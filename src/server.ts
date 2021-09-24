61ad6770df225307a1b14e7eb88469d38f7596d3
Junior <eecfredes@gmail.com>
2021-09-24 10:19:49 -0300

    feat:controllers - usercontroller

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import "reflect-metadata";
 import express from "express";
 
+import { router } from "./routes";
+
 import "./database";
 
 const PORT = "3000";
 
 const app = express();
+
+app.use(express.json());
+
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
