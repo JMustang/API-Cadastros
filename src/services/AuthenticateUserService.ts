8d248ac7de6b929fb3b2bfb2ebef387f62e9699e
Junior <eecfredes@gmail.com>
2021-10-23 21:15:16 -0300

    feat:sevices - authenticateUserService - IAuthenticateRequest -> verificar se email existe

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,10 +1,26 @@
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
 interface IAuthenticateRequest {
   email: string;
   password: string;
 }
 
 class AuthenticateUserService {
-  async execute({ email, password }: IAuthenticateRequest) {}
+  async execute({ email, password }: IAuthenticateRequest) {
+    const usersRepositories = getCustomRepository(UsersRepositories);
+
+    // Verificar se o email existe
+    const user = await usersRepositories.findOne({ email });
+
+    if (!user) {
+      throw new Error("Email/Password incorrect!");
+    }
+
+    // Verificar se a senha esta correta
+
+    // Gerar token
+  }
 }
 
 export { AuthenticateUserService };
